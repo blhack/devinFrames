953c835d6b2526cfd12b9fa231fe941d450863a4
Ryan McDermott <blhack@gmail.com>
2016-11-30 15:56:24 -0700

    Latest

--- devinFrames.ino
+++ devinFrames.ino
@@ -1,11 +1,19 @@
 #include "FastLED.h"
 #include "frameDefs.h"
+#include <NewPing.h>
+#include <RunningMedian.h>
+
 
 #define PIN 6
 #define NUM_LEDS 180
 #define NUM_FRAMES 30
-#define TRIGPIN 8
-#define ECHOPIN 9
+#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 300 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define TRIGGER_DISTANCE 20
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+RunningMedian samples = RunningMedian(1);
 
 short frameIndex = 1;           //what frame is currently being displayed
 unsigned long frameExpiration = 0;   //when shoudl that frame expire (triggering the next one)
@@ -14,6 +22,7 @@ unsigned long fadeExpiration = 0;
 short chaseDelay = 50;         //how many MS should each from be visible for
 short fadeDelay = 1;
 bool flip = false;
+int distance = 0;
 
 CRGB leds[NUM_LEDS];
 
@@ -21,19 +30,6 @@ void blackOut() {
   for (int i=0; i<NUM_LEDS; i++) {
     setPixel(i, 0,0,0);
   }
-}
-
-int getDistance() {
-  long duration, distance;
-  digitalWrite(TRIGPIN, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
-  digitalWrite(TRIGPIN, HIGH);
-  delayMicroseconds(10); // Added this line
-  digitalWrite(TRIGPIN, LOW);
-  duration = pulseIn(ECHOPIN, HIGH);
-  Serial.println(duration);
-  distance = (duration/2) / 29.1;
-  return(distance);
 }
 
 void frame(int index) {
@@ -63,10 +59,19 @@ void setPixel(int index, int r, int g, int b) {
 
 void expireFrames() {
   if (millis() > frameExpiration) {
-    frameIndex++;
+    if (flip == true) {
+      frameIndex--;
+    } else {
+      frameIndex++;
+    }
     if (frameIndex > 30) {
       frameIndex = 1;
     }
+
+    if (frameIndex < 0) {
+      frameIndex = 30;
+    }
+    
     frameExpiration = millis() + chaseDelay;
   }
 }
@@ -79,10 +84,33 @@ void expireFade() {
 }
 
 void generateFrame() {
-  if (frameIndex == 30 && flip == false) {
-    flip = true;
-  } else if (frameIndex == 30 && flip == true) {
-    flip = false;
+  //first set some defaults.  These will get overwritten by the distance sensor
+
+  flip = false;
+  chaseDelay = 50;
+
+
+  if (millis() > frameExpiration) {
+    distance = sonar.ping_cm();
+    if (distance == 0) {
+      distance = 200;
+    }
+
+
+    if (distance > 0 && distance <= 20) {
+      chaseDelay = 5000;
+    }
+
+    if (distance > 20 && distance <= 50) {
+      frameIndex = frameIndex - 2;
+    }
+    
+    if (distance > 50) {
+      frameIndex = random(0,30);
+    }
+
+    Serial.print("Distance: ");
+    Serial.println(distance);
   }
 }
 
